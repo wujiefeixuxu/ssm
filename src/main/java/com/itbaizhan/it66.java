fb69d2fd755906d72de714aeb85e91d4b295cfa5
bill <973453683@qq.com>
2023-03-24 20:08:13 +0800

    test idea git

---
+++ src/main/java/com/itbaizhan/it66.java
@@ -0,0 +1,9 @@
+package com.itbaizhan;
+
+public class it66 {
+    public static void main(String[] args) {
+        int i = 1;
+        System.out.println(i+1);
+        System.out.println("我是你爸爸");
+    }
+}
